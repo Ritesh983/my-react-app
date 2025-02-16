76546baa353f94e88167c7c5dc3bdfadb5943845
Ritesh983 <ritesh20040214@gmail.com>
2025-02-16 18:32:24 +0530

    useContext

--- 08_reactRouter/src/main.jsx
+++ 08_reactRouter/src/main.jsx
@@ -7,7 +7,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
-import Github, { githubInfoLoader } from './components/Github/github.jsx'
+import Github,{githubInfoLoader} from './components/Github/github.jsx'
 
 // const router = createBrowserRouter([
 //   {
